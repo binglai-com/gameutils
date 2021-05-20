ff1ac276ee1c7c32311a72be8292db5ce160259c
wangzijian1989 <798507577@qq.com>
2021-05-20 09:32:08 +0800

    add auth module

---
+++ auth/jwttoken.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"crypto/rsa"
+	"io/ioutil"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type RsaAuthVerifier struct {
+	RSAPubKey *rsa.PublicKey
+}
+
+//使用RSA公匙文件初始化一个校验器
+func NewRsaAuthVerifier(pubkeyfile string) (*RsaAuthVerifier, error) {
+	pem, readerr := ioutil.ReadFile(pubkeyfile)
+	if readerr != nil {
+		return nil, readerr
+	}
+
+	pubkey, parserr := jwt.ParseRSAPublicKeyFromPEM(pem)
+	if parserr != nil {
+		return nil, parserr
+	}
+
+	var ret = new(RsaAuthVerifier)
+	ret.RSAPubKey = pubkey
+	return ret, nil
+}
+
+//验证token是否有效
+func (this *RsaAuthVerifier) Verify(tokenstr string) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(tokenstr, jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) { //使用公匙解密
+		return this.RSAPubKey, nil
+	})
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/magicsea/behavior3go v0.0.0-20200622063830-4cf5449990a7
