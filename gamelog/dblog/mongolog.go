6064edea2505783b0e3cb78cbd75ce566888856b
wangzijian1989 <798507577@qq.com>
2021-07-13 16:28:21 +0800

    add some feature to dblogger

--- gamelog/dblog/mongolog.go
+++ gamelog/dblog/mongolog.go
@@ -25,10 +25,11 @@ func NewLogger(mongourl string) (*Logger, error) {
 }
 
 //记录日志
-func (l *Logger) Log(dbname string, colname string, log bson.M) {
+func (l *Logger) Log(dbname string, colname string, log bson.M) error {
 	log["time"] = time.Now().Unix()
 	if err := l.Insert(dbname, colname, log); err != nil {
 		filelog.ERROR("dblog", "log into ", dbname, ".", colname, " with ", log, " err : ", err.Error())
-		return
+		return err
 	}
+	return nil
 }
