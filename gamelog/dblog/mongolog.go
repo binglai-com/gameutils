97124aecbbd246c4eee01c812e264f7e21e9ca05
wangzijian1989 <798507577@qq.com>
2021-07-13 16:00:26 +0800

    add mongoDataBase Feature to DBLogger

--- gamelog/dblog/mongolog.go
+++ gamelog/dblog/mongolog.go
@@ -10,14 +10,14 @@ import (
 )
 
 type Logger struct {
-	db *mongo.DataBase
+	*mongo.DataBase
 }
 
 //初始化一个Logger
 func NewLogger(mongourl string) (*Logger, error) {
 	var ret = &Logger{}
-	ret.db = new(mongo.DataBase)
-	if !ret.db.Init(mongourl) {
+	ret.DataBase = new(mongo.DataBase)
+	if !ret.Init(mongourl) {
 		return nil, fmt.Errorf("dail mongo url %s fail.", mongourl)
 	}
 
@@ -27,7 +27,7 @@ func NewLogger(mongourl string) (*Logger, error) {
 //记录日志
 func (l *Logger) Log(dbname string, colname string, log bson.M) {
 	log["time"] = time.Now().Unix()
-	if err := l.db.Insert(dbname, colname, log); err != nil {
+	if err := l.Insert(dbname, colname, log); err != nil {
 		filelog.ERROR("dblog", "log into ", dbname, ".", colname, " with ", log, " err : ", err.Error())
 		return
 	}
